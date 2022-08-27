070137da33441c2d980455f7d84ec81ebdc04804
Tom Axblad <tom.axblad@gmail.com>
2022-08-27 21:51:40 +0200

    set depth to 50

--- Main.cc
+++ Main.cc
@@ -34,7 +34,7 @@ int main()
 	const int image_width = 400;
 	const int image_height = static_cast<int>(image_width / aspect_ratio);
 	const int samples_per_pixel = 100;
-	const int max_depth = 10;
+	const int max_depth = 50;
 
 	// World
 
