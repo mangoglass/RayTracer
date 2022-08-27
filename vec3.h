938a879ec66a241dc3cc9a8248ae90eb9581d17f
Tom Axblad <tom.axblad@gmail.com>
2022-08-27 21:48:32 +0200

    fix declaration error

--- vec3.h
+++ vec3.h
@@ -80,18 +80,6 @@ inline vec3 operator/(vec3 v, double t) {
     return (1/t) * v;
 }
 
-inline vec3 random_in_unit_sphere() {
-    while (true) {
-        auto p = vec3::random(-1,1);
-        if (p.length_squared() >= 1) continue;
-        return p;
-    }
-}
-
-inline vec3 random_unit_vector() {
-    return unit_vector(random_in_unit_sphere());
-}
-
 inline double dot(const vec3 &u, const vec3 &v) {
     return u.e[0] * v.e[0] + u.e[1] * v.e[1] + u.e[2] * v.e[2]; 
 }
@@ -104,6 +92,18 @@ inline vec3 cross(const vec3 &u, const vec3 &v) {
 
 inline vec3 unit_vector(vec3 v) {
     return v / v.length();
+}
+
+inline vec3 random_in_unit_sphere() {
+    while (true) {
+        auto p = vec3::random(-1,1);
+        if (p.length_squared() >= 1) continue;
+        return p;
+    }
+}
+
+inline vec3 random_unit_vector() {
+    return unit_vector(random_in_unit_sphere());
 }
 
 // Type aliases for vec3
