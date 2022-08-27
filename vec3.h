545650f114ecacc920b0a41f5cb0d44b0bbfcaa7
Tom Axblad <tom.axblad@gmail.com>
2022-08-27 21:44:40 +0200

    add lambertian reflection

--- vec3.h
+++ vec3.h
@@ -88,6 +88,10 @@ inline vec3 random_in_unit_sphere() {
     }
 }
 
+inline vec3 random_unit_vector() {
+    return unit_vector(random_in_unit_sphere());
+}
+
 inline double dot(const vec3 &u, const vec3 &v) {
     return u.e[0] * v.e[0] + u.e[1] * v.e[1] + u.e[2] * v.e[2]; 
 }
