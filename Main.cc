94d3963c83c55b1a68fe03e6d3d1aa0bdb92e835
Tom Axblad <tom.axblad@gmail.com>
2022-09-09 20:27:53 +0200

    Add fov to camera definition

--- Main.cc
+++ Main.cc
@@ -36,6 +36,7 @@ color ray_color(const ray& r, const hittable& world, int depth) {
 int main() {
 	// Image 
 
+	const auto vertical_fov = 90.0;
 	const auto aspect_ratio = 16.0 / 9.0;
 	const int image_width = 400;
 	const int image_height = static_cast<int>(image_width / aspect_ratio);
@@ -44,22 +45,30 @@ int main() {
 
 	// World
 
+	auto R = cos(pi / 4);
 	hittable_list world;
 
-	auto material_ground = make_shared<lambertian>(color(0.8, 0.8, 0.0));
-	auto material_center = make_shared<lambertian>(color(0.1, 0.2, 0.5));
-	auto material_left   = make_shared<dielectric>(1.5);
-	auto material_right  = make_shared<metal>(color(0.8, 0.6, 0.2), 1.0);
+	auto material_left  = make_shared<lambertian>(color(0, 0, 1));
+	auto material_right = make_shared<lambertian>(color(1, 0, 0));
 
-	world.add(make_shared<sphere>(point3( 0.0, -100.5, -1.0), 100.0, material_ground));
-	world.add(make_shared<sphere>(point3( 0.0,    0.0, -1.0),   0.5, material_center));
-	world.add(make_shared<sphere>(point3(-1.0,    0.0, -1.0),   0.5, material_left));
-	world.add(make_shared<sphere>(point3(-1.0,    0.0, -1.0),  -0.4, material_left));
-	world.add(make_shared<sphere>(point3( 1.0,    0.0, -1.0),   0.5, material_right));
+	// auto material_ground = make_shared<lambertian>(color(0.8, 0.8, 0.0));
+	// auto material_center = make_shared<lambertian>(color(0.1, 0.2, 0.5));
+	// auto material_left   = make_shared<dielectric>(1.5);
+	// auto material_right  = make_shared<metal>(color(0.8, 0.6, 0.2), 1.0);
+
+	world.add(make_shared<sphere>(point3(-R, 0, -1), R, material_left));
+	world.add(make_shared<sphere>(point3( R, 0, -1), R, material_right));
+
+
+	// world.add(make_shared<sphere>(point3( 0.0, -100.5, -1.0), 100.0, material_ground));
+	// world.add(make_shared<sphere>(point3( 0.0,    0.0, -1.0),   0.5, material_center));
+	// world.add(make_shared<sphere>(point3(-1.0,    0.0, -1.0),   0.5, material_left));
+	// world.add(make_shared<sphere>(point3(-1.0,    0.0, -1.0),  -0.4, material_left));
+	// world.add(make_shared<sphere>(point3( 1.0,    0.0, -1.0),   0.5, material_right));
 
 	// Camera
 
-	camera cam;
+	camera cam(vertical_fov, aspect_ratio);
 
 	// Render
 
